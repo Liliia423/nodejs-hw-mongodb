b902821788e8d702fdc91eb8787bcf78c50eee8d
Liliia423 <etude4408@gmail.com>
2025-07-16 19:39:05 +0300

    Додала swagger-документацію та оновила upload.js

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -5,7 +5,6 @@ import multer from 'multer';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
 import { v2 as cloudinary } from 'cloudinary';
 
-console.log(process.env.CLOUDINARY_API_KEY);
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
